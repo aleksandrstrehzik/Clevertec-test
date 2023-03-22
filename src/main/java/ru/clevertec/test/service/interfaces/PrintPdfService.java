99e7e28f2186771a88165ea5d3bd12a53b5b6dbe
aleksandrstrehzik <aleksandrstrehzik@gmail.com>
2023-03-22 20:45:19 +0300

    add: pdf print for check

---
+++ src/main/java/ru/clevertec/test/service/interfaces/PrintPdfService.java
@@ -0,0 +1,10 @@
+package ru.clevertec.test.service.interfaces;
+
+import com.itextpdf.text.DocumentException;
+
+import java.io.IOException;
+
+public interface PrintPdfService {
+
+    void printPdf();
+}
